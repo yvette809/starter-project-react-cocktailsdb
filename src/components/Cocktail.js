02a71aa4f2a3ce7d48020f9039c256602f83f680
Yvette <nchombuayvta@gmil.com>
2020-08-09 22:55:11 +0200

    details page

--- src/components/Cocktail.js
+++ src/components/Cocktail.js
@@ -1,5 +1,19 @@
 import React from "react";
+import {Link} from 'react-router-dom'
 
-export default function Cocktail() {
-  return <h1>cocktail component</h1>;
+export default function Cocktail({item}) {
+ const {image,name,id,info,glass} = item
+  return(
+    <article className="cocktail">
+      <div className= "img-container">
+        <img src = {image} alt ={name}/>
+      </div>
+      <div className = "cocktail-footer">
+        <h3>{name}</h3>
+        <h4>{glass}</h4>
+        <p>{info}</p>
+        <Link to= {`/cocktail/${id}`} className="btn btn-primary btn-details">details</Link>
+        </div>
+    </article>
+  )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,51 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import CocktailList from "../components/CocktailList"
+import SearchForm from "../components/SearchForm"
 
 export default function Home() {
-  return <h1>error page</h1>;
+
+  const [loading,setLoading] = useState(false)
+  const [searchTerm,setSearchTerm] = useState("a")
+  const [cocktails,setCocktails] = useState([])
+
+  // useEffect (()=>{
+  //   fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchTerm}`).then(response=>response.json())
+  //   .then(data => setCocktails(data.drinks))
+  // }, [searchTerm])
+
+  useEffect(()=>{
+    setLoading(true)
+      async function getDrinks(){
+        try{
+          const response = await fetch (`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchTerm}`)
+          const data = await response.json()
+           const {drinks} = data;
+           if(drinks){
+
+            const newCocktails = drinks.map(item=> {
+            const {idDrink,strDrink,strDrinkThumb, strAlcoholic, strGlass}= item
+              return {id:idDrink, name:strDrink, image:strDrinkThumb, info:strAlcoholic, glass:strGlass}
+            })
+
+            setCocktails(newCocktails)
+           
+            
+           }else{
+             setCocktails([])
+           }
+
+        }catch(error){
+          console.log(error)
+        }
+        setLoading(false)
+      }
+      getDrinks()
+  },[searchTerm])
+
+  return(
+    <main>
+      <SearchForm setSearchTerm={setSearchTerm}/>
+      <CocktailList loading ={loading} cocktails={cocktails}/>
+    </main>
+  ) 
 }
