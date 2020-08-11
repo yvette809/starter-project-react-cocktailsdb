788e7e05b941c672d584e4374a29df6ed8095684
Yvette <nchombuayvta@gmil.com>
2020-08-11 19:56:53 +0200

    prjt completed

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,5 +1,33 @@
-import React from "react";
+import React, {useRef,useEffect} from "react";
 
-export default function SearchForm() {
-  return <h1>searchform component</h1>;
+export default function SearchForm({setSearchTerm}) {
+
+  const searchValue = useRef("");
+  console.log(searchValue)
+
+  const handleSubmit = (e)=>{
+    e.preventDefault()
+  }
+
+  useEffect(()=>{
+    searchValue.current.focus()
+
+  },[])
+
+  const searchCocktail =()=>{
+    setSearchTerm(searchValue.current.value)
+  }
+
+  return (
+    <section>
+      <h2 className="section-title">Search Cocktails</h2>
+      <form className="form search-form" onSubmit ={handleSubmit}></form>
+      <div className="form-control">
+        <label htmlFor="name">
+        Search Your favorite cocktail
+        </label>
+        <input type="text" name="name" id="name" onChange={searchCocktail} ref ={searchValue}/>
+      </div>
+    </section>
+  )
 }
